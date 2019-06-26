a824b995839609255291c164f19e0f0200823bdf
Ronen Lapushner <ronenk17@gmail.com>
2019-06-26 21:41:56 +0300

    Removed C ide dependency

--- init.el
+++ init.el
@@ -1,16 +1,14 @@
 ;; RONEN'S EMACS CONFIG
 ;; -----------------------
-;; Last change: 25/06/2019
+;; Last change: 26/06/2019
 
 ;; Add MELPA
 (require 'package)
 (add-to-list 'package-archives
          '("melpa" . "http://melpa.org/packages/") t)
 
-;; Do not check package signatures + show line numbers
+;; Do not check package signatures
 (setq package-check-signature nil)
-(when (version<= "26.0.50" emacs-version )
-  (global-display-line-numbers-mode))
 
 ;; Initialize all packages
 (package-initialize)
@@ -23,18 +21,6 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
-
-(add-to-list 'load-path "~/.emacs.d/custom")
-
-(require 'setup-general)
-(if (version< emacs-version "24.4")
-    (require 'setup-ivy-counsel)
-  (require 'setup-helm)
-  (require 'setup-helm-gtags))
- ;;(require 'setup-ggtags)
-(require 'setup-cedet)
-(require 'setup-editing)
-
 
 ;; function-args
 ;; (require 'function-args)
@@ -49,9 +35,10 @@
  '(custom-safe-themes
    (quote
     ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(org-agenda-files (quote ("~/Documents/test.org")))
  '(package-selected-packages
    (quote
-    (beacon spacemacs-theme function-args company-c-headers sr-speedbar zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
+    (org-bullets projectile helm-projectile helm-make helm beacon function-args use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -59,58 +46,8 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; helm-gtags
-(setq
- helm-gtags-ignore-case t
- helm-gtags-auto-update t
- helm-gtags-use-input-at-cursor t
- helm-gtags-pulse-at-cursor t
- helm-gtags-prefix-key "\C-cg"
- helm-gtags-suggested-key-mapping t
- )
-
-(require 'helm-gtags)
-;; Enable helm-gtags-mode
-(add-hook 'dired-mode-hook 'helm-gtags-mode)
-(add-hook 'eshell-mode-hook 'helm-gtags-mode)
-(add-hook 'c-mode-hook 'helm-gtags-mode)
-(add-hook 'c++-mode-hook 'helm-gtags-mode)
-(add-hook 'asm-mode-hook 'helm-gtags-mode)
-
-(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
-(define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
-(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
-(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
-(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
-
-;; Configure company
-(require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
-
 ;; Load the spacemacs-dark theme
 (load-theme 'spacemacs-dark t)
 
-;; Enable menubar
-(menu-bar-mode 1)
-
-;; Default shell for terminal,
-;; and don't ask before exiting it.
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-(defvar default-term-shell "/bin/bash")
-(defadvice ansi-term (before force-bash)
-  (interactive (list default-term-shell)))
-(ad-activate 'ansi-term)
-
-;; Bind ansi-term to super+enter
-(global-set-key (kbd "<s-return>") 'ansi-term)
-
-;; Disable the annoying bell shit
-(setq ring-bell-function 'ignore)
-
-;; Beacon line highlight
-(use-package beacon
-  :ensure t
-  :init
-  (beacon-mode 1))
+;; Load the actual config file
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
